da2c91e59fa7854b01efa385560ddddd423c8a70
csili2025 <csili1414@gmail.com>
2025-07-25 10:44:17 +0300

    unit test 2

--- bibliogradle/bibliogradle/src/test/java/edu/itplus/bibliosping/backend/utils/PasswordHasherTest.java
+++ bibliogradle/bibliogradle/src/test/java/edu/itplus/bibliosping/backend/utils/PasswordHasherTest.java
@@ -2,11 +2,28 @@ package edu.itplus.bibliosping.backend.utils;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.*;
 
 class PasswordHasherTest {
 
     @Test
     void hashPassword() {
+    }
+
+    @Test
+    void hashPassword_givenDifferentInput_shouldReturnDifferentHash() {
+        // Arrange
+        PasswordHasher sut = new PasswordHasher();
+        String password = "abcde";
+        String salt = UUID.randomUUID().toString();
+
+        // Act
+        String hash = sut.hashPassword(password, salt);
+
+        // Assert
+        String expectedHash = "123456";
+        assertThat(hash).isEqualTo(expectedHash);
     }
 }
