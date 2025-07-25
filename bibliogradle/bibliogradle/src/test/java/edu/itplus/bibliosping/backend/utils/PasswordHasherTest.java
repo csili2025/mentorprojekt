2b7213b9808deac88b6bec1be06daabc1813889c
csili2025 <csili1414@gmail.com>
2025-07-25 11:05:32 +0300

    unit passed

--- bibliogradle/bibliogradle/src/test/java/edu/itplus/bibliosping/backend/utils/PasswordHasherTest.java
+++ bibliogradle/bibliogradle/src/test/java/edu/itplus/bibliosping/backend/utils/PasswordHasherTest.java
@@ -17,13 +17,13 @@ class PasswordHasherTest {
         // Arrange
         PasswordHasher sut = new PasswordHasher();
         String password = "abcde";
-        String salt = UUID.randomUUID().toString();
+        String salt = "44a3415d-65f1-44c8-aa7f-c62ed4c50675";
 
         // Act
         String hash = sut.hashPassword(password, salt);
 
         // Assert
-        String expectedHash = "123456";
+        String expectedHash = "D02D1748B55B82DF438C1C6A933D63AED8FDE20CEF6C6CDA6D1879623E8DE81B";
         assertThat(hash).isEqualTo(expectedHash);
     }
 }
